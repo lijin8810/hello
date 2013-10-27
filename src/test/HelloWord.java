4a4aba1ae20647f8c27c148723522fe88068cef9
lijin8810 <lijin8810@gmail.com>
2013-10-27 17:26:05 +0800

    test

--- src/test/HelloWord.java
+++ src/test/HelloWord.java
@@ -7,7 +7,7 @@ public class HelloWord {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello Word!");
+		System.out.println("Hello Word!!!");
 	}
 
 }
